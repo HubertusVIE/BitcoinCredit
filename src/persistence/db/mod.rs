6147d34f5792509b021e2e9baaad0ceb68ad0d5e
tompro <office@protom.eu>
2024-11-22 12:42:31 +0100

    SurrealDb contact store

--- src/persistence/db/mod.rs
+++ src/persistence/db/mod.rs
@@ -6,8 +6,20 @@ use surrealdb::{
 
 pub mod contact;
 
-pub async fn get_surreal_db(connection_string: &str, namespace: &str) -> Result<Surreal<Any>> {
+#[allow(dead_code)]
+pub async fn get_surreal_db(
+    connection_string: &str,
+    namespace: &str,
+    database: &str,
+) -> Result<Surreal<Any>> {
     let db = connect(connection_string).await?;
-    db.use_ns(namespace).await?;
+    db.use_ns(namespace).use_db(database).await?;
+    Ok(db)
+}
+
+#[cfg(test)]
+pub async fn get_memory_db(namespace: &str, database: &str) -> Result<Surreal<Any>> {
+    let db = connect("mem://").await?;
+    db.use_ns(namespace).use_db(database).await?;
     Ok(db)
 }
