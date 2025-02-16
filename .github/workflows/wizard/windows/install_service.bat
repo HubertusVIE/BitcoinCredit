aa5aca1bcb206788d7fec9ee99f4a0eb61a3f8cb
djekmusic <im@bitcr.org>
2025-02-16 13:15:00 +0100

    Installers v0.2.0

--- .github/workflows/wizard/windows/install_service.bat
+++ .github/workflows/wizard/windows/install_service.bat
@@ -1,6 +1,6 @@
-./nssm.exe install "%2" "%1\bitcredit.exe" "--http-port %3 --p2p-port 0 --surreal-db-connection rocksdb://data/surreal --nostr-relay wss://bitcr-cloud-run-03-550030097098.europe-west1.run.app --bitcoin-network mainnet"
-./nssm.exe set "%2" DisplayName "Bitcredit Core"
-./nssm.exe set "%2" AppStdout "%1\logs\stdout.txt"
-./nssm.exe set "%2" AppStderr "%1\logs\stderr.txt"
-./nssm.exe set "%2" AppRestartDelay 300000
-./nssm.exe start "%2" "SERVICE_AUTO_START"
+nssm.exe install "%1" "%CD%\bitcredit.exe" "--http-port %2 --p2p-port 0 --surreal-db-connection rocksdb://data/surreal --nostr-relay wss://bitcr-cloud-run-03-550030097098.europe-west1.run.app --bitcoin-network mainnet"
+nssm.exe set "%1" DisplayName "Bitcredit Core"
+nssm.exe set "%1" AppStdout "%CD%\logs\stdout.txt"
+nssm.exe set "%1" AppStderr "%CD%\logs\stderr.txt"
+nssm.exe set "%1" AppRestartDelay 300000
+nssm.exe start "%1" "SERVICE_AUTO_START"
